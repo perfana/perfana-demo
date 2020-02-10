d89ef56f99a4f7615ad73cf33cf51b6cf03d4ba7
Daniel Moll <daniel@perfana.io>
2020-02-10 22:02:10 +0100

    removed unused files

--- perfana-fixture/helpers/datasources/elastic.js
+++
@@ -1,9 +0,0 @@
-const yves = require('yves')
-yves.debugger('Y', { stream: null, sortKeys: true, hideFunctions: true, singleLineMax: 0, obfuscates: [/key/i, /token/i] })
-const config = require('config')
-if (config.debug) {
-  yves.debugger().enable(config.debug)
-}
-
-yves.debugger(config.debugPrefix)
-
