6b462d3f8976158d6513d33b5f75879ec6f0b05b
Daniel Moll <daniel@perfana.io>
2020-02-14 16:18:38 +0100

    Bumped perfana version

--- export-realm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker exec -ti perfana-test-env_keycloak_1 /opt/jboss/keycloak/bin/standalone.sh \
-	-Djboss.socket.binding.port-offset=1000 \
-	-Dkeycloak.migration.action=export \
-	-Dkeycloak.migration.provider=singleFile \
-	-Dkeycloak.migration.realmName=perfana \
-	-Dkeycloak.migration.usersExportStrategy=REALM_FILE  \
-	-Dkeycloak.migration.file=/tmp/keycloak/keycloak-perfana-realm.json
