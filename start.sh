d78999d7b3e3648ab49ec2361220bffa8a64bb8d
Daniel Moll <daniel@perfana.io>
2020-11-18 14:51:05 +0100

    removed mariadb and omnidb

--- start.sh
+++ start.sh
@@ -10,7 +10,6 @@ sleep 1
 $DOCKER_CMD run -ti --rm -v $CONFIG_FILE:/init-mongo.js --net perfana-blog_perfana mongo:4.4-rc /usr/bin/mongo --host mongo1 --port 27011 /init-mongo.js
 
 echo "Bringing up databases that need a little bit more time to start up..."
-docker-compose --compatibility up -d mariadb
 docker-compose --compatibility up -d influxdb
 
 echo "Starting Grafana ..."
@@ -45,7 +44,6 @@ docker-compose --compatibility up -d perfana-snapshot
 docker-compose --compatibility up -d perfana-check
 docker-compose --compatibility up -d telegraf
 docker-compose --compatibility up -d wiremock
-docker-compose --compatibility up -d omnidb
 docker-compose --compatibility up -d prometheus
 docker-compose --compatibility up -d alertmanager
 docker-compose --compatibility up -d jaeger
