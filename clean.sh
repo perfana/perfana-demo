d60b16744edf494f6089cfc1d11589dff2165a87
Daniel Moll <dmoll1974@gmail.com>
2025-03-04 15:55:18 +0100

    fix regex for restoring pom when cleaning env

--- clean.sh
+++ clean.sh
@@ -4,4 +4,4 @@ source common.sh
 
 docker-compose down -v
 
-sed -i '' 's|<apiKey>[^<]*</apiKey>|<apiKey>__apiKey__</apiKey>|' loadtest/pom.xml
+sed -i '' 's|<apiKey>[^$][^<]*</apiKey>|<apiKey>__apiKey__</apiKey>|' loadtest/pom.xml
