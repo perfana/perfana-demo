54942cb891dcb1c4246b74dac8a434fb8503c115
Daniel Moll <dmoll1974@gmail.com>
2025-05-24 16:19:02 +0200

    chore: add linux support

--- mongodb/init-mongo.js
+++ mongodb/init-mongo.js
@@ -1,8 +1,8 @@
 rs.initiate({
     _id: "rs0",
     members: [
-        { _id: 0, host: "host.docker.internal:27011" },
-        { _id: 1, host: "host.docker.internal:27012" },
-        { _id: 2, host: "host.docker.internal:27013" }
+        { _id: 0, host: "mongo1:27011" },
+        { _id: 1, host: "mongo2:27012" },
+        { _id: 2, host: "mongo3:27013" }
     ]
 });
